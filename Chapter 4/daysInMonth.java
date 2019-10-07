007c640b70f8be59ba7a8306d8f8858930be752f
hhd626 <56241054+hhd626@users.noreply.github.com>
2019-10-08 07:17:24 +0800

    Create daysInMonth.java

---
+++ Chapter 4/daysInMonth.java
@@ -0,0 +1,9 @@
+public static int daysInMonth(int month) {
+    if (month == 2) {
+        return 28;
+    } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+        return 30;
+    } else {
+        return 31;
+    }
+}
