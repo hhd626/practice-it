d241108f4825a56b8b4a301e599b91128f1295cb
hhd626 <56241054+hhd626@users.noreply.github.com>
2019-10-08 08:38:13 +0800

    Create Self-Check 2.21: Count2 .java

---
+++ Chapter 2/Self-Check 2.21: Count2 .java
@@ -0,0 +1,14 @@
+/*
+replace FINISH ME with own code to get following output
+2 times 1 = 2
+2 times 2 = 4
+2 times 3 = 6
+2 times 4 = 8
+*/
+public class Count2 {
+    public static void main(String[] args) {
+        for (int i = 1; i<=4; i++) {
+            System.out.println("2 times " + i + " = " + (2*i));
+        }
+    }
+}
